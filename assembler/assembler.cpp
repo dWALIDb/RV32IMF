cc424ac312e882fb06b735586df1a66001347e4c
dWALIDb <brazzawalidmokrane@gmail.com>
2024-09-10 12:07:14 +0100

    Add files via upload

---
+++ assembler/assembler.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "assembler_class.hpp"
+
+int main(){
+
+    assembler  a;
+    
+    a.assemble("C:\\Users\\brazz\\OneDrive\\Bureau\\learn\\risc-v assembly\\instruction file.txt",
+    "C:\\Users\\brazz\\OneDrive\\Bureau\\learn\\risc-v assembly\\output_file");
+
+
+
+    return 0;
+}
